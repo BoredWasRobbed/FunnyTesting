a77247d723de805e3556cded5d9c64e1ec3bc97f
BoredWasRobbed <bored7160@gmail.com>
2026-06-12 02:48:57 -0400

    Updated ReplicatedStorage/NetRay/Types/BrickColor.lua

---
+++ ReplicatedStorage/NetRay/Types/BrickColor.lua
@@ -0,0 +1,19 @@
+-- @ScriptType: ModuleScript
+--!strict
+--!optimize 2
+--!native
+-- Requires
+local Cursor = require(script.Parent.Cursor)
+
+return {
+
+	Read = function(cursor: Cursor.Cursor)
+		return BrickColor.new(cursor:ReadU3())
+	end,
+
+	Write = function(cursor: Cursor.Cursor, value: BrickColor)
+		cursor:Allocate(3)
+		cursor:WriteU3(value.Number)
+	end,
+
+}
