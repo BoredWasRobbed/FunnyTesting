544e2d4c55fd007bfffebe09d13c1f76ea4e1177
BoredWasRobbed <bored7160@gmail.com>
2026-06-17 17:55:24 -0400

    Delete StarterPlayer/StarterPlayerScripts directory

--- StarterPlayer/StarterPlayerScripts/Client.lua
+++
@@ -1,4 +0,0 @@
--- @ScriptType: LocalScript
-local CharacterService = require(script.Parent:WaitForChild("ClientModules"):WaitForChild("CharacterService"))
-
-CharacterService.init()
--- StarterPlayer/StarterPlayerScripts/Client/ClientData.lua
+++
@@ -1,5 +0,0 @@
--- @ScriptType: LocalScript
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local DataService = require(ReplicatedStorage.DataService).client
-
-DataService:init()
