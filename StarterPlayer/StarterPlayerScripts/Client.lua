9a97b6a88591c6e8220ef7692d45d61dcc0491dd
BoredWasRobbed <bored7160@gmail.com>
2026-06-17 18:51:33 -0400

    Updated StarterPlayer/StarterPlayerScripts/Client.lua

--- StarterPlayer/StarterPlayerScripts/Client.lua
+++ StarterPlayer/StarterPlayerScripts/Client.lua
@@ -2,13 +2,10 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterPlayerScripts = game:GetService("StarterPlayer").StarterPlayerScripts
 
-local DataService = require(ReplicatedStorage:WaitForChild("DataService")).client
-
 local ClientModules = StarterPlayerScripts:WaitForChild("ClientModules")
 local UIHandler = require(ClientModules:WaitForChild("UIHandler"))
 
 local GlobalModules = ReplicatedStorage:WaitForChild("GlobalModules")
-local CharacterService = require(GlobalModules:WaitForChild("CharacterService"))
 
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
 local SwitchEvent = Remotes:WaitForChild("SwitchCharacter")
@@ -17,6 +14,6 @@ local player = game.Players.LocalPlayer
 
 UIHandler.constructCharacterList()
 
-SwitchEvent.OnClientEvent:Connect(function(characterMoveset)
-	UIHandler.constructMoveset(player, characterMoveset)
+SwitchEvent.OnClientEvent:Connect(function(characterData)
+	UIHandler.constructMoveset(player, characterData.Moveset)
 end)
