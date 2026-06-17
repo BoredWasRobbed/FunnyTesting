97512436715d465a9a00d06c907a84af1a9432d9
BoredWasRobbed <bored7160@gmail.com>
2026-06-17 18:19:17 -0400

    Updated StarterPlayer/StarterPlayerScripts/ClientModules/UIHandler.lua

---
+++ StarterPlayer/StarterPlayerScripts/ClientModules/UIHandler.lua
@@ -0,0 +1,58 @@
+-- @ScriptType: ModuleScript
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Assets = ReplicatedStorage:WaitForChild("Assets")
+local UI = Assets:WaitForChild("UI")
+
+local GlobalModules = ReplicatedStorage:WaitForChild("GlobalModules")
+local CharacterService = require(GlobalModules:WaitForChild("CharacterService"))
+
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+local SwitchEvent = Remotes:WaitForChild("SwitchCharacter")
+
+local Icon = require(ReplicatedStorage:WaitForChild("Icon"))
+
+local UIHandler = {}
+
+local function insertHotbarItem(player: Player, item: {}, layoutOrder: number)
+	if not item then return end
+	
+	if item.Type == "BaseMove" then
+		local newMove = UI.MoveTemplate:Clone()
+		newMove.Bind.Text = item.Bind or tostring(layoutOrder)
+		newMove.MoveName.Text = item.Name or "N/A"
+		newMove.Tooltip.Text = item.Tooltip or ""
+		newMove.LayoutOrder = layoutOrder or tonumber(item.Bind)
+		newMove.Parent = player:WaitForChild("PlayerGui"):WaitForChild("HUD").MovesetContainer.Hotbar
+	end
+end
+
+function UIHandler.constructCharacterList()
+	local characterList = {}
+
+	for characterName, _ in pairs(CharacterService:getCharacters()) do
+		local newIcon = Icon.new()
+		newIcon:setLabel(characterName)
+		newIcon:bindEvent("toggled", function()
+			SwitchEvent:FireServer(characterName)
+		end)
+
+		table.insert(characterList, newIcon)
+	end
+	
+	Icon.new()
+		:setName("CharacterList")
+		:setLabel("Characters")
+		:setDropdown(characterList)
+end
+
+
+function UIHandler.constructMoveset(player, characterMoveset)
+	for _, v in pairs(characterMoveset) do
+		if v.Type == "BaseMove" then
+			insertHotbarItem(player, v)
+		end
+	end
+end
+
+return UIHandler
