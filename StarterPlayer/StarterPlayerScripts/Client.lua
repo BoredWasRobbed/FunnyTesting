c85bac304120f456829ae636572e542ca82253ad
BoredWasRobbed <bored7160@gmail.com>
2026-06-20 09:04:32 -0400

    Updated StarterPlayer/StarterPlayerScripts/Client.lua

--- StarterPlayer/StarterPlayerScripts/Client.lua
+++ StarterPlayer/StarterPlayerScripts/Client.lua
@@ -7,11 +7,20 @@ local UIHandler = require(ClientModules:WaitForChild("UIHandler"))
 
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
 local SwitchEvent = Remotes:WaitForChild("SwitchCharacter")
+local RequestCharacterEvent = Remotes:WaitForChild("RequestCharacter")
 
 local player = game.Players.LocalPlayer
 
 UIHandler.constructCharacterList()
 
 SwitchEvent.OnClientEvent:Connect(function(characterData)
+	if characterData.Initial then
+		UIHandler.applyCharacterBars(player, characterData.Name)
+		UIHandler.constructMoveset(player, characterData.Moveset)
+		return
+	end
+
 	UIHandler.transitionCharacter(player, characterData.Name, characterData.Moveset)
 end)
+
+RequestCharacterEvent:FireServer()
