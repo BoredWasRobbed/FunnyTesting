d8f89505a7872c00495ba2187cb6d0facf2a037d
BoredWasRobbed <bored7160@gmail.com>
2026-06-22 13:17:52 -0400

    Delete ReplicatedStorage/GlobalModules directory

--- ReplicatedStorage/GlobalModules/SkillSystem/GetsugaTenshoAir.lua
+++
@@ -1,8 +0,0 @@
--- @ScriptType: ModuleScript
-local GetsugaTenshoAir = {}
-
-function GetsugaTenshoAir.Play(player: Player, context)
-	print(`Playing {context.VariantName} variant of {context.Move.Name} for {player.Name}`)
-end
-
-return GetsugaTenshoAir
--- ReplicatedStorage/GlobalModules/SkillSystem/SanktBogen.lua
+++
@@ -1,8 +0,0 @@
--- @ScriptType: ModuleScript
-local SanktBogen = {}
-
-function SanktBogen.Play(player: Player, context)
-	print(`Playing {context.VariantName} variant of {context.Move.Name} for {player.Name}`)
-end
-
-return SanktBogen
